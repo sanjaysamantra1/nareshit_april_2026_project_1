bd1f93cffde1ac7f2a136dfac1767941167555b5
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-14 09:03:05 +0530

    lifecycle methods

--- src/app/components/child1-demo/child1-demo.ts
+++ src/app/components/child1-demo/child1-demo.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child1-demo',
   imports: [],
   templateUrl: './child1-demo.html',
   styleUrl: './child1-demo.css',
-  inputs:['a','b']
+  inputs: ['a', 'b', 'numArr'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Child1Demo {
-  a:any;
-  b:any;
+  a: any;
+  b: any;
+  numArr: any;
+  courseName: string;
+
+  async fetchUserData() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let userDate = await response.json();
+    console.log(userDate);
+  }
+
+  constructor(
+    private ele: ElementRef,
+    private cdr: ChangeDetectorRef,
+  ) {
+    // Dependency Injection
+    console.log('child constructor');
+    this.courseName = 'Angular';
+  }
+  ngOnInit() {
+    console.log('child ngOnInit');
+    this.fetchUserData();
+  }
+  ngOnChanges(myChanges: SimpleChanges) {
+    console.log('child ngOnChanges');
+    console.log(myChanges);
+  }
+  ngDoCheck() {
+    console.log('child ngDoCheck');
+    this.cdr.markForCheck();
+  }
+  // ngAfterContentInit() {
+  //   console.log('child ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('child ngAfterContentChecked');
+  // }
+  // ngAfterViewInit() {
+  //   console.log('child ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('child ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('child ngOnDestory');
+  // }
 }
--- src/app/components/parent-demo/parent-demo.ts
+++ src/app/components/parent-demo/parent-demo.ts
@@ -1,18 +1,48 @@
 import { Child2Demo } from './../child2-demo/child2-demo';
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { Child1Demo } from '../child1-demo/child1-demo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parent-demo',
-  imports: [Child1Demo, Child2Demo],
+  imports: [Child1Demo, Child2Demo, FormsModule],
   templateUrl: './parent-demo.html',
   styleUrl: './parent-demo.css',
 })
 export class ParentDemo {
+  numArr = [10, 20, 30, 40];
   a = 100;
   c_parent: number | undefined;
 
   receiveDataFromChild2(c_child2: number) {
     this.c_parent = c_child2;
   }
+
+  constructor() {
+    console.log('Parent constructor');
+  }
+  // ngOnChanges() {
+  //   console.log('Parent ngOnChanges');
+  // }
+  ngOnInit() {
+    console.log('Parent ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('Parent ngDoCheck');
+  }
+  // ngAfterContentInit() {
+  //   console.log('Parent ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('Parent ngAfterContentChecked');
+  // }
+  // ngAfterViewInit() {
+  //   console.log('Parent ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('Parent ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('Parent ngOnDestory');
+  // }
 }
